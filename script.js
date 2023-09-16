1ff0e2740a8d2e09863486492d39d68876edd480
Khurram Shahzad <106273270+khurramshahzadlali@users.noreply.github.com>
2023-09-16 23:27:46 +0500

    Updated Taxes

--- script.js
+++ script.js
@@ -1,16 +1,4 @@
 // script.js
-function calculate1() {
-    var Kg = parseFloat(document.getElementById('Kg').value);
-    var kgPrice = parseFloat(document.getElementById('kgPrice').value);
-    var duty = parseFloat(document.getElementById('duty').value);
-    var freight = parseFloat(document.getElementById('freight').value);
-    var extExp = parseFloat(document.getElementById('extExp').value);
-  
-    var totalCost = Kg * kgPrice + duty*kgPrice/100 + freight + extExp;
-  
-    document.getElementById('result').textContent = totalCost;
-  }
-  
   function calculate() {
     var widthInches = parseFloat(document.getElementById('widthInches').value);
     var heightMM = parseFloat(document.getElementById('heightMM').value);
@@ -28,3 +16,32 @@ function calculate1() {
     document.getElementById('lengthCal').textContent = rollWeight.toFixed(2)  + " Kgs";
   }
   
+  function calculate1() {
+    var Kg = parseFloat(document.getElementById('Kg').value);
+    var kgPrice = parseFloat(document.getElementById('kgPrice').value);
+    var freight = parseFloat(document.getElementById('freight').value);
+    var extExp = parseFloat(document.getElementById('extExp').value);
+    var duty = parseFloat(document.getElementById('custom-duty').value);
+    var extDuty = parseFloat(document.getElementById('additional-duty').value);
+    var saleTax = parseFloat(document.getElementById('sales-tax').value);
+    var additionalSaleTax = parseFloat(document.getElementById('additional-sales-tax').value);
+    var incomeTaxRate = parseFloat(document.getElementById('income-tax').value);
+
+    var unitCost = kgPrice + freight + extExp;
+    var dutyCalculations = unitCost * (duty + extDuty) / 100;
+
+    var totalCost = unitCost + dutyCalculations;
+    
+    var salesTax = totalCost * (saleTax + additionalSaleTax) / 100;
+
+    var totalcostinclSalesTax = totalCost + salesTax;
+
+    var incomeTax = totalcostinclSalesTax * incomeTaxRate / 100;
+
+    var totalCost1 = totalcostinclSalesTax + incomeTax;
+
+  
+    
+  
+    document.getElementById('result').textContent = Kg*totalCost1.toFixed(2);
+  }
